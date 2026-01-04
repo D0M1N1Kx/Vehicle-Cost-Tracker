db4b62f73f7cc0e7c38c3ef9d540d4c184707591
D0M1N1K_. <milkovicsd32@gmail.com>
2026-01-04 13:21:08 +0100

    i called getters from serviceViewModel instead of local methods

--- lib/pages/service_log_page.dart
+++ lib/pages/service_log_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:vehicle_cost_tracker_app/models/vehicle.dart';
+import 'package:vehicle_cost_tracker_app/viewmodels/service_view_model.dart';
 
 class ServiceLogPage extends StatefulWidget {
   final Vehicle vehicle;
@@ -18,103 +20,98 @@ class _ServiceLogPageState extends State<ServiceLogPage> {
     _car = widget.vehicle;
   }
 
-  int get totalAllCosts {
-    int total = 0;
-
-    if (_car.services != null) {
-      for (var service in _car.services!) {
-        total += service.cost;
-      }
-    }
-
-    if (_car.refuels != null) {
-      for (var refuel in _car.refuels!) {
-        total += refuel.fuelCost;
-      }
-    }
-
-    return total;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: ListTile(
-          title: Text(
-            '${_car.brand} ${_car.modell}',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          subtitle: Text('Service Log'),
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16.0),
+    return ChangeNotifierProvider(
+      create: (_) => ServiceViewModel(_car),
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: false,
+            title: ListTile(
+              title: Text(
+                '${_car.brand} ${_car.modell}',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
               ),
-              elevation: 2.0,
-              child: Column(
-                children: [
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              subtitle: Text('Service Log'),
+            ),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  elevation: 2.0,
+                  child: Column(
                     children: [
-                      Column(
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Icon(
-                            Icons.build_circle_outlined,
-                            color: Colors.orange,
+                          Column(
+                            children: [
+                              Icon(
+                                Icons.build_circle_outlined,
+                                color: Colors.orange,
+                              ),
+                              Text(
+                                context
+                                    .read<ServiceViewModel>()
+                                    .allServicesAndRefuelsCount
+                                    .toString(),
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text('All services'),
+                            ],
                           ),
-                          Text(
-                            (_car.services?.length ?? 0).toString(),
-                            style: TextStyle(fontWeight: FontWeight.bold),
+                          SizedBox(width: 20),
+                          Column(
+                            children: [
+                              Icon(Icons.wallet, color: Colors.lightGreen),
+                              Text(
+                                context
+                                    .read<ServiceViewModel>()
+                                    .totalCosts
+                                    .toString(),
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text('All costs'),
+                            ],
                           ),
-                          Text('All services'),
                         ],
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(
+                            Colors.orange,
+                          ),
+                        ),
+                        child: Text(
+                          '+ New service log',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
                       ),
                       SizedBox(width: 20),
-                      Column(
-                        children: [
-                          Icon(Icons.wallet, color: Colors.lightGreen),
-                          Text(
-                            totalAllCosts.toString(),
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text('All costs'),
-                        ],
-                      ),
                     ],
                   ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all(Colors.orange),
-                    ),
-                    child: Text(
-                      '+ New service log',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                ],
-              ),
+                ),
+                SizedBox(height: 50),
+              ],
             ),
-            SizedBox(height: 50),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
